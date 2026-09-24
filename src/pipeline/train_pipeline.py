2637e25c50564a0e6c5b523641558d77d2203f00
Shrey1418 <shreyaggrawal18@gmail.com>
2026-09-24 22:19:40 +0530

    end-to-end training  pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,1 +1,16 @@
-    
+import sys
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.exception import CustomException
+from src.logger import logging
+
+if __name__ == "__main__":
+    try:
+        train_path, test_path = DataIngestion().initiate_data_ingestion()
+        train_arr, test_arr, preprocessor_path = DataTransformation().initiate_data_transformation(train_path, test_path)
+        score = ModelTrainer().initiate_model_training(train_arr, test_arr)
+        logging.info(f"Triang pipeline completed sucessfuly, final PR-AUC: {score:.4f}")
+        print(f"Training complete. PR-AUC: {score:.4f}")
+    except Exception as e:
+        raise CustomException(e, sys)
